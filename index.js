4de4b84f0561445dc521440a2dd76c67274f963f
akashbhar19 <akashbhar19@gmail.com>
2018-12-08 19:19:55 -0500

    Map now loads in div

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-function draw(geo_data) {
-	"use strict";
-	var width = 750,
-		height = 600,
-		scale = 6500,
-		translateX = -240,
-		translateY = 7260;
-  
-	var projection = d3.geo.mercator()
-		.scale(scale)
-		.translate( [translateX, translateY]);
-  
-	var path = d3.geo.path().projection(projection);
-  
-	var svg = d3.select("#chart")
-		.append("svg")
-		  .attr("width", width)
-		  .attr("height", height)
-		  .attr("class", "map");
-  
-	var map = svg.selectAll("path")
-		.data(geo_data.features)
+var currentKey = 'CMPD_Divison';
+
+var width = 900,
+	height = 600;
+
+//SVG element using width and height
+var svg = d3.select('#map')
+	.append('svg')
+	.attr('width', width)
+	.attr('height', height);
+
+var projection = d3.geo.mercator()
+	.scale(40000)
+	.center([-80.8040009, 35.26374625])
+	.translate([width/2, height/2]);
+
+var path = d3.geo.path()
+	.projection(projection);
+
+d3.json('data/traffic_data_CMPD.geojson', function(error, features) {
+	svg.append('g')
+		.attr('class', 'features')
+		.selectAll('path')
+		.data(features.features)
 		.enter()
-		.append("path")
-		  .attr("d", path)
-		  .style("fill", "lightBlue")
-		  .style("stroke", "black")
-		  .style("stroke-width", 0.5);
-  };
+		.append('path')
+		.attr('d', path);
+});
