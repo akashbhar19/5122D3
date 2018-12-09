ad6fbe707440bd60d1ead9ceee904b7a48aa0116
akashbhar19 <akashbhar19@gmail.com>
2018-12-08 19:40:49 -0500

    Added Dynamic Resize

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ var svg = d3.select('#map')
 	.attr('height', height);
 
 var projection = d3.geo.mercator()
-	.scale(40000)
-	.center([-80.8040009, 35.26374625])
-	.translate([width/2, height/2]);
+	.scale(1);
 
 var path = d3.geo.path()
 	.projection(projection);
 
 d3.json('data/traffic_data_CMPD.geojson', function(error, features) {
+	var scaleCenter = calculateScaleCenter(features);
+
+	projection.scale(scaleCenter.scale)
+		.center(scaleCenter.center)
+		.translate([width/2, height/2]);
+
 	svg.append('g')
 		.attr('class', 'features')
 		.selectAll('path')
@@ -26,3 +30,26 @@ d3.json('data/traffic_data_CMPD.geojson', function(error, features) {
 		.append('path')
 		.attr('d', path);
 });
+
+function calculateScaleCenter(features) {
+	// Get the bounding box of the paths (in pixels!) and calculate a
+	// scale factor based on the size of the bounding box and the map
+	// size.
+	var bbox_path = path.bounds(features),
+		scale = 0.95 / Math.max(
+		  (bbox_path[1][0] - bbox_path[0][0]) / width,
+		  (bbox_path[1][1] - bbox_path[0][1]) / height
+		);
+  
+	// Get the bounding box of the features (in map units!) and use it
+	// to calculate the center of the features.
+	var bbox_feature = d3.geo.bounds(features),
+		center = [
+		  (bbox_feature[1][0] + bbox_feature[0][0]) / 2,
+		  (bbox_feature[1][1] + bbox_feature[0][1]) / 2];
+  
+	return {
+	  'scale': scale,
+	  'center': center
+	};
+  }
