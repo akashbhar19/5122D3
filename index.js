3dfb72b1ccd1e2aeee947157e6575bd5cbea11a5
akashbhar19 <akashbhar19@gmail.com>
2018-12-09 20:16:06 -0500

    Added Tooltips and dynamic domain resizing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var currentKey = 'CMPD_Divison';
+var currentKey = 'count';
 
 var width = 900,
 	height = 600;
@@ -12,16 +12,28 @@ var svg = d3.select('#map')
 var projection = d3.geo.mercator()
 	.scale(1);
 
+var zoom = d3.behavior.zoom()
+	.scaleExtent([1, 10])
+	.on('zoom', doZoom);
+svg.call(zoom);
+
 var path = d3.geo.path()
 	.projection(projection);
 
-var dataById, countData = new Array();
+var tooltip = d3.select("#map")
+	.append("div")
+	.attr("class", "tooltip hidden");
 
-var nameCounter = 0;
+
+var dataById, countData = new Array(), tempArr = new Array(), results = new Array(), resultsCount = new Array();
+
+var nameCounter = 0, lowest = Number.POSITIVE_INFINITY, highest = Number.NEGATIVE_INFINITY;
 
 var quantize = d3.scale.quantize()
-	.domain([0, 10000])
-	.range(d3.range(9).map(function (i) { return 'q' + i + '-9'; }));
+	.range(d3.range(12).map(function (i) { return 'q' + i + '-12'; }));
+
+var mapFeatures = svg.append('g')
+	.attr('class', 'features YlGnBu');
 
 d3.json('data/traffic_data_CMPD.geojson', function (error, features) {
 	var scaleCenter = calculateScaleCenter(features);
@@ -35,15 +47,45 @@ d3.json('data/traffic_data_CMPD.geojson', function (error, features) {
 			.key(function (d) { return d.CMPD_Division; }).entries(data);
 		//console.log(dataById);
 		dataById.forEach(element => {
-			countData.push({ id: element.key, count: element.values.length });
+			element.values.forEach(obj => {
+				tempArr.push(obj.Result_of_Stop);
+			});
+			for (i = 0; i < tempArr.length; i++) {
+				if (results.includes(tempArr[i])) {
+					resultsCount[results.indexOf(tempArr[i])]++;
+				}
+				else {
+					results.push(tempArr[i]);
+					resultsCount.push(1);
+				}
+			}
+			tempArr = new Array();
+			for (i = 0; i < results.length; i++) {
+				tempArr.push({ result: results[i], count: resultsCount[i] });
+			}
+			countData.push({ id: element.key, count: element.values.length, results: tempArr });
+			tempArr = new Array();
+			results = new Array();
+			resultsCount = new Array();
 		});
-		console.log(dataById);
 
+		for (i = 0; i < countData.length; i++) {
+			for (j = 0; j < countData[i].results.length; j++) {
+				var temp = countData[i].results[j].count;
+				if (temp < lowest) {
+					lowest = temp;
+				}
+				if (temp > highest) {
+					highest = temp;
+				}
+			}
+		}
 
-		console.log(countData);
+		quantize.domain([
+			lowest, highest
+		]);
 
-		svg.append('g')
-			.attr('class', 'features YlGnBu')
+		mapFeatures
 			.selectAll('path')
 			.data(features.features)
 			.enter()
@@ -51,15 +93,21 @@ d3.json('data/traffic_data_CMPD.geojson', function (error, features) {
 			.attr('class', function (d) {
 				//console.log(d.properties.dname, nameCounter);
 				for (i = 0; i < countData.length; i++) {
-					if(countData[i].id == d.properties.dname){
-						console.log(d.properties.dname, countData[i].count, "yay");
-						return quantize(countData[i].count);
+					if (countData[i].id == getIdOfFeature(d)) {
+						console.log(countData[i].results[1].count);
+						return quantize(getValueOfData(countData[i]));
 					}
 				}
 			})
-			.attr('d', path);
+			.attr('d', path)
+			.on('mousemove', showTooltip)
+			.on('mouseout', hideTooltip);;
 	});
 });
+
+function getValueOfData(d) {
+	return +d[currentKey];
+}
 
 function calculateScaleCenter(features) {
 	// Get the bounding box of the paths (in pixels!) and calculate a
@@ -83,3 +131,38 @@ function calculateScaleCenter(features) {
 		'center': center
 	};
 }
+
+function doZoom() {
+	mapFeatures.attr("transform",
+		"translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")")
+		// Keep the stroke width proportional. The initial stroke width
+		// (0.5) must match the one set in the CSS.
+		.style("stroke-width", 0.5 / d3.event.scale + "px");
+}
+
+function getIdOfFeature(f) {
+	return f.properties.dname;
+}
+
+function showTooltip(f) {
+	// Get the ID of the feature.
+	var id = getIdOfFeature(f);
+	var d = dataById[id];
+	var mouse = d3.mouse(d3.select('#map').node()).map(
+		function (d) { return parseInt(d); }
+	);
+	
+	var left = Math.min(width - 4 * id.length, mouse[0] + 5);
+	
+	var top = mouse[1] + 25;
+	console.log(id);
+	// Use the ID to get the data entry.
+	// Show the tooltip (unhide it) and set the name of the data entry.
+	tooltip.classed('hidden', false)
+    .attr("style", "left:" + left + "px; top:" + top + "px")
+    .html(id);
+}
+
+function hideTooltip() {
+	tooltip.classed('hidden', true);
+  }
